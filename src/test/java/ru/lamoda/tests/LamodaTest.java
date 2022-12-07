0878ad41174e3b2be1ceb2123bb5f9ce9d3876c8
Kirill <kirillcnv@gmail.com>
2022-12-07 10:59:55 +0300

    refactoring

--- src/test/java/ru/lamoda/tests/LamodaTest.java
+++ src/test/java/ru/lamoda/tests/LamodaTest.java
@@ -1,8 +1,6 @@
 package ru.lamoda.tests;
 
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.*;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,8 +9,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import ru.lamoda.pages.LamodaTestPage;
 import ru.lamoda.utils.TestConfig;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 @Feature("Проверка сайта https://www.lamoda.ru/")
@@ -64,7 +60,7 @@ public class LamodaTest extends TestConfig {
 
     @DisplayName("Проверка поисковой строки")
     @ValueSource(strings = {"Футболка", "Джинсы", "Парка"})
-    @ParameterizedTest(name = "Результаты поиска выдают квартиры рядом с \"{0}\"")
+    @ParameterizedTest(name = "Результаты поиска выдают \"{0}\"")
     public void searchLamoda(String testData) {
         step("Открыть главную страницу", () -> {
             lamodaTestPage.openPage();
